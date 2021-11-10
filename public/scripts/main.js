c557cb23f700d0252b556fa867c28e7a8b9dc812
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-11-10 17:55:15 +0100

    more yea

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -9,24 +9,48 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./source/index.js":
-/*!*************************!*\
-  !*** ./source/index.js ***!
-  \*************************/
-/***/ (() => {
+/***/ "./source/scripts/index.js":
+/*!*********************************!*\
+  !*** ./source/scripts/index.js ***!
+  \*********************************/
+/***/ ((module) => {
 
-eval("console.log('yea');\n\n\n//# sourceURL=webpack://v-core9_com/./source/index.js?");
+eval("const v9 = {\n  config: null,\n  data: null,\n  init(){\n    console.log(\"YEA v_core9\");\n  }\n};\n\nv9.init();\n\nmodule.exports = v9;\n\n\n//# sourceURL=webpack://v-core9_com/./source/scripts/index.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./source/index.js"]();
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__("./source/scripts/index.js");
 /******/ 	
 /******/ })()
 ;
