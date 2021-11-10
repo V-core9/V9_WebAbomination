829b85f0ead5943647d778962e4867414d729fdc
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-11-10 07:46:40 +0100

    yea many new things

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("YEA");
+var path = require('path');
+var express = require('express');
+var app = express();
+
+var htmlPath = path.join(__dirname, 'public');
+
+app.use(express.static(htmlPath));
+
+var server = app.listen(2000, function () {
+    var host = 'localhost';
+    var port = server.address().port;
+    console.log('listening on http://'+host+':'+port+'/');
+});
+
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  entry: './source/index.js',
+  mode: "development",
+  target: "web",
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+};
