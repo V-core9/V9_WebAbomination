0be97f1372b18ee65d6b1db3ba7c1dcce65ff7a4
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-12-11 01:25:36 +0100

    yea

---
+++ system/setup/index.js
@@ -0,0 +1,15 @@
+const v_db = require('../../v_db');
+
+v_db.type.new('users');
+v_db.type.new('pages');
+v_db.type.new('posts');
+v_db.type.new('comments');
+v_db.type.new('tags');
+v_db.type.new('categories');
+v_db.type.new('settings');
+v_db.type.new('roles');
+v_db.type.new('permissions');
+v_db.type.new('blacklist_ip');
+v_db.type.new('whitelist_ip');
+
+
