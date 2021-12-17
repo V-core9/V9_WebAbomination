5e67d996bc217fce677012e1a9bbf5a044c618af
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-12-17 05:08:35 +0100

    yea

--- system/$_APP/v_cache.js
+++ system/$_APP/v_cache.js
@@ -1,6 +1,8 @@
 const v_cache = {
     refresh_time: 1000,
+
     _items: {},
+
     get: async (key) => {
         try {
             return (v_cache._items[key] !== undefined) ? v_cache._items[key] : false;
@@ -8,9 +10,11 @@ const v_cache = {
             return false;
         }
     },
+
     save: async (key, value) => {
         v_cache._items[key] = { value: value, time: Date.now() };
     }
+    
 };
 
 module.exports = v_cache;
