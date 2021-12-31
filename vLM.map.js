07989e56958563141378041d90b335d3c77ad24e
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-12-31 09:44:54 +0100

    yea

---
+++ vLM.map.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const v_lightmapper = require('v_lightmapper');
+
+const config = {
+    protocol: "https",
+    host: "v-core9.com",
+    path: "sitemap_index.xml",
+    reportsDir : path.join(__dirname, `reports/`),
+    save_to_file: true,
+    disableHeadlessMode: false,
+    onlyCategories : ["performance"]
+};
+
+v_lightmapper(config);
