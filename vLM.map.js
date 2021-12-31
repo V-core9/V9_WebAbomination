adf0ee7dba71721778245f90885d81fa53d31c85
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-12-31 13:30:07 +0100

    yea

--- vLM.map.js
+++ vLM.map.js
@@ -4,9 +4,6 @@ const vTables = require('./system/config/tables');
 const vFS = require('v_file_system');
 const vDB = require('v_database');
 
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-
 const config = {
     protocol: "https",
     host: "v-core9.com",
@@ -16,23 +13,6 @@ const config = {
     disableHeadlessMode: false,
     onlyCategories : ["performance"]
 };
-
-/*
-(async() => {
-
-    const cfgFilePath = config.reportsDir+'/v-core9.com.json';
-    if (await vFS.isFile(cfgFilePath)) await vFS.deleteFile(config.reportsDir+'/v-core9.com.json');
-
-    await v_lightmapper(config);
-
-    const data = JSON.parse(await vFS.read('./system/INSTALL/pages/lightmap.json'));
-    console.log(data);
-
-    data.sections[1].data = await vFS.read('./vLM_reports/v-core9.com.json');
-
-    console.log("SAVING : " + await vDB.item.new(vTables.pages, JSON.stringify(data), 'lightmap'));
-})();
-*/
 
 (async() => {
 
@@ -45,7 +25,7 @@ const config = {
         console.log("IS DONE?");
         if (await vFS.isFile(cfgFilePath)) {
             const data = JSON.parse(await vFS.read('./system/INSTALL/pages/lightmap.json'));
-            data.sections[1].data = JSON.parse(await vFS.read('./vLM_reports/v-core9.com.json'));
+            data.sections[1].data = JSON.parse(await vFS.read(cfgFilePath));
             await vDB.item.new(vTables.pages, data, 'lightmap');
             console.log('YEA');
             clearInterval(waitForJSON_Interval);
