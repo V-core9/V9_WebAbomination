b3b3dfb6363c46f0eb06b7d2b78ef0166585bdee
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-01-24 00:38:19 +0100

    yea

--- api_src/vLM.js
+++ api_src/vLM.js
@@ -16,30 +16,26 @@ const vlm_cfg = {
     //onlyCategories : ["performance"]
 };
 
-var waitForJSON_Interval = null;
-
-const waitForJSON_Stop = () => {
-    console.log('DONE ALL!');
-    clearInterval(waitForJSON_Interval);            
-    waitForJSON_Interval = null;
-};
-
-const mainLoop = async() => {
-    if (await vFS.isFile(cfgFilePath)) {
-
-        const data = await vDB.item.view('pages', 'lightmap');
-        data.sections[1].data = JSON.parse(await vFS.read(cfgFilePath));
-
-        await vDB.item.new(tables.pages, data, 'lightmap');
-
-        waitForJSON_Stop();
-    }
-};
-
-const cfgFilePath = vlm_cfg.reportsDir+'/'+vlm_cfg.host+'.json';
-
 (async() => {
+
+    const cfgFilePath = vlm_cfg.reportsDir+'/'+vlm_cfg.host+'.json';
     if (await vFS.isFile(cfgFilePath)) await vFS.deleteFile(vlm_cfg.reportsDir+'/v-core9.com.json');
+
     await v_lightmapper(vlm_cfg);
-    waitForJSON_Interval = setInterval(mainLoop, 5000);
+
+    var waitForJSON_Interval = setInterval(async() => {
+        if (await vFS.isFile(cfgFilePath)) {
+
+            const data = await vDB.item.view('pages', 'lightmap');
+            data.sections[1].data = JSON.parse(await vFS.read(cfgFilePath));
+
+            await vDB.item.new(tables.pages, data, 'lightmap');
+
+            console.log('DONE ALL!');
+
+            clearInterval(waitForJSON_Interval);
+            
+            waitForJSON_Interval = null;
+        }
+    }, 5000);
 })();
