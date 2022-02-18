97a0bcc6d2996fc1c028f861f8152526380810b0
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-02-18 17:40:53 +0100

    yea fix minor

--- __tests__/DEMO/app.002/actions.js
+++ __tests__/DEMO/app.002/actions.js
@@ -1,19 +1,20 @@
 const { asy } = require('../../../helpers');
 const models = require('../../../models');
 
-//! Prisma Client
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const pageModel = new models.Page();
+const postModel = new models.Page();
+const userModel = new models.Page();
+
 
 //* Just some functions that use prisma.
 //* Still just testing of random things.
 module.exports = {
-  getHomepage: async (req, res) => res.status(200).send(await asy.stringifyJSON(await models.page.home())),
-  getPageBySlug: async (req, res) => res.status(200).end(await asy.stringifyJSON(await models.page.byArgs(req.params))),
-  listPages: async (req, res) => res.status(200).end(await asy.stringifyJSON(await models.page.all())),
+  getHomepage: async (req, res) => res.status(200).send(await asy.stringifyJSON(await pageModel.home())),
+  getPageBySlug: async (req, res) => res.status(200).end(await asy.stringifyJSON(await pageModel.byArgs(req.params))),
+  listPages: async (req, res) => res.status(200).end(await asy.stringifyJSON(await pageModel.all())),
 
-  getBlog: async (req, res) => res.status(200).end(await asy.stringifyJSON(await models.post.all())),
+  getBlog: async (req, res) => res.status(200).end(await asy.stringifyJSON(await postModel.all())),
 
-  userList: async (req, res) => res.status(200).end(await asy.stringifyJSON(await models.user.all())),
-  userById: async (req, res) => res.status(200).end(await asy.stringifyJSON(await models.user.byId(await asy.parseInt(req.params.id)))),
+  userList: async (req, res) => res.status(200).end(await asy.stringifyJSON(await userModel.all())),
+  userById: async (req, res) => res.status(200).end(await asy.stringifyJSON(await userModel.byId(await asy.parseInt(req.params.id)))),
 };
--- models/page/index.js
+++ models/page/index.js
@@ -3,6 +3,6 @@ module.exports = class Page extends Base {
   constructor(app) {
     super(app);
     this.type = 'page';
-    this.home = async () => await one(this.type, { slug: '/' });
+    this.home = async () => await this.byArgs({ slug: '/' });
   }
 };
