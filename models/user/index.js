0593a2116bd134baf5c792d829167cc8013e4cd1
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-02-19 12:05:03 +0100

    yea

--- models/user/index.js
+++ models/user/index.js
@@ -2,11 +2,14 @@ module.exports = class User extends require('../base') {
   constructor() {
     super();
     this.type = 'user';
+
     this.register = async ( data ) => {
-      console.log(await this.create({data}))
-    },
+      console.log(await this.create({data}));
+    };
+
     this.login = async (username, password) => {
       return await this.byArgs({username: username});
-    }
+    };
+
   }
 };
