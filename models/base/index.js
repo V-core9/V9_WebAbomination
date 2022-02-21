178bb8d60a61446ad2c5f3f03f56ae7eca3c6f99
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-02-21 18:29:42 +0100

    linting

--- models/base/index.js
+++ models/base/index.js
@@ -17,20 +17,20 @@ module.exports = class Base {
       }
     };
 
-      this.update = async (id, data) => (await prisma[this.type].update({ where: { id: id }, data: data })) ? true : false;
+    this.update = async (id, data) => (await prisma[this.type].update({ where: { id: id }, data: data })) ? true : false;
 
-      this.delete = async (id) => {
-        try {
-          return await prisma[this.type].delete({ where: { id: id } });
-        } catch (err) {
-          //console.log(err);
-          return false;
-        }
-      };
+    this.delete = async (id) => {
+      try {
+        return await prisma[this.type].delete({ where: { id: id } });
+      } catch (err) {
+        //console.log(err);
+        return false;
+      }
+    };
 
-      this.byArgs = async (args) => await prisma[this.type].findFirst({ where: args });
+    this.byArgs = async (args) => await prisma[this.type].findFirst({ where: args });
 
-      this.purge = async () => (await prisma[this.type].deleteMany({})) ? true : false;
+    this.purge = async () => (await prisma[this.type].deleteMany({})) ? true : false;
 
-    }
-  };
+  }
+};
