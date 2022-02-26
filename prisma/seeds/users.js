5b9d41fcec178b73e58ca7007dac0f6cc6022bae
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-02-26 22:19:47 +0100

    yea trying something out

--- prisma/seeds/users.js
+++ prisma/seeds/users.js
@@ -1,25 +1,22 @@
 const v_to_sha256 = require('v_to_sha256');
-module.exports = users = [];
-
-(async () => {
-
-  //! #1 user prep [ v-core9 ]
-  users.push({
+const { saltGenerator } = require('../../helpers');
+module.exports = users = [
+  {
     email: 'slavko.vuletic92@gmail.com',
     username: 'v-core9',
-    password: await v_to_sha256('01234567890123456789'),
-    salt: '0123456789',
+    password: '0123456789',
     roleId: 1
-  });
-
-  //! #2 user prep [ ana_v9 ]
-  users.push({
+  },
+  {
     email: 'blagojevic021@gmail.com',
     username: 'ana_v9',
-    password: await v_to_sha256('01234567890123456789'),
-    salt: '0123456789',
+    password: '0123456789',
     roleId: 2
-  });
+  }
+];
 
-})();
-
+users.map(async (user) => {
+  user.salt = saltGenerator();
+  user.password = await v_to_sha256(user.password + user.salt);
+  await prisma.user.create({ data: user });
+});
