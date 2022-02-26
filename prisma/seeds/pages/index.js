e2f4e5099683610bec67b324332beeba4e6d1906
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-02-26 21:53:38 +0100

    yea splitting prisma into files, shitty solution I know...but somehow there is no way yet to split the schema even though I\'ve already managed to see schemas that are over 4k lines long...had to split it somehow so we dont end up with similar situation down the line

--- prisma/seeds/pages/index.js
+++ prisma/seeds/pages/index.js
@@ -9,7 +9,7 @@ module.exports = [
     metaDescription: 'V-core9 Starting Point and How to Stay on Top of the Situation while looking confused.',
     metaKeywords: 'V-core9, Development, Web, Tools, APIs, Blog, News, Tech, Hire, SEO, Performance',
     thumbnail: '/images/favicon.png',
-    content: fs.readFileSync(path.join(__dirname, './pages/homepage.html'), 'utf8'),
+    content: fs.readFileSync(path.join(__dirname, './homepage.html'), 'utf8'),
     renderMode: 'html',
     published: true
   },
@@ -21,7 +21,7 @@ module.exports = [
     metaDescription: 'V-core9 Starting Point and How to Stay on Top of the Situation while looking confused.',
     metaKeywords: 'V-core9, Development, Web, Tools, APIs, Blog, News, Tech, Hire, SEO, Performance',
     thumbnail: '/images/favicon.png',
-    content: fs.readFileSync(path.join(__dirname, './pages/about-us.html'), 'utf8'),
+    content: fs.readFileSync(path.join(__dirname, './about-us.html'), 'utf8'),
     renderMode: 'html',
     published: true
   },
@@ -33,7 +33,7 @@ module.exports = [
     metaDescription: 'V-core9 Starting Point and How to Stay on Top of the Situation while looking confused.',
     metaKeywords: 'Contact, Message, Signal, Ask Question, V-core9',
     thumbnail: '/images/favicon.png',
-    content: fs.readFileSync(path.join(__dirname, './pages/contact-us.html'), 'utf8'),
+    content: fs.readFileSync(path.join(__dirname, './contact-us.html'), 'utf8'),
     renderMode: 'html',
     published: true
   },
@@ -45,7 +45,7 @@ module.exports = [
     metaDescription: 'V-core9 Starting Point and How to Stay on Top of the Situation while looking confused.',
     metaKeywords: 'Sign-in, Login, Authentication, Confirmation, V-core9',
     thumbnail: '/images/favicon.png',
-    content: fs.readFileSync(path.join(__dirname, './pages/login.html'), 'utf8'),
+    content: fs.readFileSync(path.join(__dirname, './login.html'), 'utf8'),
     renderMode: 'html',
     published: true
   },
@@ -57,7 +57,7 @@ module.exports = [
     metaDescription: 'Best page to start your journey with V-core9 and all the wonders it brings.',
     metaKeywords: 'Register, Account, Profile, V-core9, Development, Web, Tools, APIs, JoinUs',
     thumbnail: '/images/favicon.png',
-    content: fs.readFileSync(path.join(__dirname, './pages/register.html'), 'utf8'),
+    content: fs.readFileSync(path.join(__dirname, './register.html'), 'utf8'),
     renderMode: 'html',
     published: true
   },
