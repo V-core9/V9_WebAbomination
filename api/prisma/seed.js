db9fc3898faf3fc5f9984d771929a010278914f5
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-03-26 16:53:50 +0100

    comments, few fixes, seeds...etc.

--- api/prisma/seed.js
+++ api/prisma/seed.js
@@ -7,24 +7,22 @@ var objListNames = Object.keys(seeds);
 console.log(objListNames);
 
 (async () => {
-  console.log("Started V-core9 Database Seeding >>>");
+  console.log("🌱 Started V-core9 Database Seeding >>>");
 
   for (var i = 0; i < objListNames.length; i++) {
     var objName = objListNames[i];
-    console.log(objName);
-    console.log("1. CleanUp : Deleting " + objName);
+    
+    console.log(i + ". Cleaning & Seeding: " + objName);
     console.log(await prisma[objName].deleteMany());
 
-
-    console.log("2. Seeding Data: " + objName);
     var objList = seeds[objName];
     for (var j = 0; j < objList.length; j++) {
       var obj = objList[j];
-      await prisma[objName].create({data: obj});
+      await prisma[objName].create({ data: obj });
     }
   }
 
-  console.log("3. DONE >> Proceeding to exit, Bye!");
+  console.log("🚀 DONE >> Proceeding to exit, Bye!");
   console.log(await prisma.$disconnect());
   process.exit(1);
 })();
