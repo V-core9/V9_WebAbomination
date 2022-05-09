e604f59aae6f1d154fa3711a81ce0d5ce378a16f
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-05-09 21:06:20 +0200

    yea

--- app/routes/users.js
+++ app/routes/users.js
@@ -6,8 +6,8 @@ const prisma = new PrismaClient();
 /* GET users listing. */
 router.get('/', async (req, res, next) => {
   try {
-    var data = await prisma.user.findMany({ take: 10, orderBy: { id: 'desc' }, select: { id: true, username: true, email: true, role: true } });
-    res.render('users', { data: data});
+    var data = await prisma.user.findMany({ take: 10, orderBy: { id: 'desc' }, select: { id: true, username: true, email: true, role: true, created: true, updated: true, active: true } });
+    res.render('users', { data: data });
   } catch (error) {
     return res.status(400).json(error);
   }
@@ -16,8 +16,8 @@ router.get('/', async (req, res, next) => {
 /* GET user by id. */
 router.get('/:username', async (req, res, next) => {
   try {
-    var data = await prisma.user.findUnique({ where: { username: req.params.username }, select: { id: true, username: true, email: true, role: true } });
-    res.render('user', { data: data});
+    var data = await prisma.user.findUnique({ where: { username: req.params.username }, select: { id: true, username: true, email: true, role: true, created: true, updated: true, active: true } });
+    res.render('user', { data: data });
   } catch (error) {
     return res.status(400).json(error);
   }
@@ -26,8 +26,8 @@ router.get('/:username', async (req, res, next) => {
 /* GET user by id. */
 router.get('/byId/:id', async (req, res, next) => {
   try {
-    var data = await prisma.user.findUnique({ where: { id: parseInt(req.params.id) }, select: { id: true, username: true, email: true, role: true } });
-    res.render('user', { data: data});
+    var data = await prisma.user.findUnique({ where: { id: parseInt(req.params.id) }, select: { id: true, username: true, email: true, role: true, created: true, updated: true, active: true } });
+    res.render('user', { data: data });
   } catch (error) {
     return res.status(400).json(error);
   }
